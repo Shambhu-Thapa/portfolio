2c21c93d7ec951f2f3a29f3d0d39b7ed03f51fce
Shambhu Thapa <51013975+Shambhu-Thapa@users.noreply.github.com>
2021-02-01 23:38:59 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,77 @@
+var dob = new Date("04/22/1998");  
+    //calculate month difference from current date in time  
+    var month_diff = Date.now() - dob.getTime();  
+      
+    //convert the calculated difference in date format  
+    var age_dt = new Date(month_diff);   
+      
+    //extract year from date      
+    var year = age_dt.getUTCFullYear();  
+      
+    //now calculate the age of the user  
+    var age = Math.abs(year - 1970);  
+    
+    document.getElementById('age').innerHTML=age;
+  
+
+
+
+var d = new Date();
+  var currentYear = d.getFullYear();
+document.getElementById('copyrightYear').innerHTML=currentYear;
+
+
+
+
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 500px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+setInterval(showTime, 1000); 
+function showTime() { 
+    let time = new Date(); 
+    let hour = time.getHours(); 
+    let min = time.getMinutes(); 
+    let sec = time.getSeconds(); 
+    am_pm = "AM"; 
+
+    if (hour > 12) { 
+        hour -= 12; 
+        am_pm = "PM"; 
+    } 
+    if (hour == 0) { 
+        hr = 12; 
+        am_pm = "AM"; 
+    } 
+
+    hour = hour < 10 ? "0" + hour : hour; 
+    min = min < 10 ? "0" + min : min; 
+    sec = sec < 10 ? "0" + sec : sec; 
+
+    let currentTime = hour + ":"  
+        + min + ":" + sec + am_pm; 
+
+    document.getElementById("clock") 
+        .innerHTML = currentTime; 
+} 
+
+showTime();
